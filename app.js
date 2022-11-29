92f6e8a551c096dd01ae37e51fa1fd849140cc9a
hvkst <bernd@hvkst.de>
2022-11-29 16:45:36 +0100

    add basic setup

---
+++ app.js
@@ -0,0 +1,19 @@
+// Basic setup copied from ironlauncher
+
+require('dotenv').config();
+
+require('./db');
+
+const express = require('express');
+
+const app = express();
+
+require('./config')(app);
+
+// 👇 Start handling routes here
+const indexRoutes = require('./routes/index.routes');
+app.use('/api', indexRoutes);
+
+require('./error-handling')(app);
+
+module.exports = app;
